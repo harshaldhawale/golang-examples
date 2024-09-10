4eaf7c6daaf4ac2e5f60db99a55bb50b718ead32
harshal <harshal.dhawale@gmail.com>
2024-09-10 17:24:53 +0530

    pointers and structs completed

---
+++ pointer.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+/*
+Go supports pointers, allowing you to pass references to values and records within your program.
+*/
+
+func zeroval(ival int){
+	ival = 0
+}
+
+func zeroptr(iptr *int){
+	*iptr = 0
+}
+
+func main(){
+	i := 1
+	fmt.Println("initial:", i)
+
+	zeroval(i)
+	fmt.Println("zeroval:", i)
+
+	zeroptr(&i)
+	fmt.Println("zeroptr:", i)
+
+	fmt.Println("pointer:", &i)
+ 
+}
